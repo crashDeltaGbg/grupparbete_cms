7aaace3a22c2f80caa9b08315822519c5e101962
Jon RICKARD Yxelflod <rickard.yxelflod@gmail.com>
2021-11-30 11:10:44 +0100

    add open positions posts to career page

--- page-career.php
+++ page-career.php
@@ -1,17 +1,26 @@
 <?php
 get_header(); ?>
 
-<?php
-// query_posts(array('category_name' => 'positions', 'posts_per_page' => -1));
-$query = new WP_Query('cat=-!1');
 
-if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
-    <div>
-      <h2><?php the_title(); ?></h2>
-      <div><?php the_content(); ?></div>
-    </div>
-<?php endwhile;
-endif; ?>
+
+<div class="career">
+  <h1><?php the_title(); ?></h1>
+  <?php the_content(); ?>
+  <?php
+  // query_posts(array('category_name' => 'positions', 'posts_per_page' => -1));
+  $query = new WP_Query('cat=-!1');
+
+  if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+      <div>
+        <h2><?php the_field('position_title'); ?></h2>
+        <div>
+          <?php the_field('position_details'); ?>
+        </div>
+      </div>
+  <?php endwhile;
+  endif; ?>
+
+</div>
 
 <?php
 get_footer(); ?>
