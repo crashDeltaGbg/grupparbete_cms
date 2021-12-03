d8277e0779c8c41496d11b5fbf15bcd52652d5b9
Jon RICKARD Yxelflod <rickard.yxelflod@gmail.com>
2021-12-03 12:47:23 +0100

    update css

--- functions.php
+++ functions.php
@@ -1,34 +1,36 @@
 <?php
+
 /**
  * GeneratePress child theme functions and definitions.
  *
  * Add your custom PHP in this file.
  * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
  */
-function generatepress_child_enqueue_scripts() {
-	if ( is_rtl() ) {
-		wp_enqueue_style( 'generatepress-rtl', trailingslashit( get_template_directory_uri() ) . 'rtl.css' );
-	}
+function generatepress_child_enqueue_scripts()
+{
+  if (is_rtl()) {
+    wp_enqueue_style('generatepress-rtl', trailingslashit(get_template_directory_uri()) . 'rtl.css');
+  }
 }
-add_action( 'wp_enqueue_scripts', 'generatepress_child_enqueue_scripts', 100 );
+add_action('wp_enqueue_scripts', 'generatepress_child_enqueue_scripts', 100);
 
 
 
-function allow_onclick_content() {
+function allow_onclick_content()
+{
   global $allowedposttags, $allowedtags;
   $newattribute = "onclick";
 
   $allowedposttags["a"][$newattribute] = true;
   $allowedtags["a"][$newattribute] = true; //unnecessary?
 }
-add_action( 'init', 'allow_onclick_content' );
+add_action('init', 'allow_onclick_content');
 
 
 
-register_sidebar( array(
-   'name'          => 'Footer Widget',
-   'id'            => 'footer-widget',
-   'before_widget' => '<div class="footer-widget">',
-   'after_widget'  => '</div>'
-) );
-
+register_sidebar(array(
+  'name'          => 'Footer Widget',
+  'id'            => 'footer-widget',
+  'before_widget' => '<div class="footer-widget">',
+  'after_widget'  => '</div>'
+));
